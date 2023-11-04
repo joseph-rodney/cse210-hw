c3d7a27ecfaca3708c9e48e4d1cb7d3ace12c9b5
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-04 22:03:21 +0300

    finished Prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string mark = Console.ReadLine();
+
+        int percentage = int.Parse(mark);
+
+        string grade = "";
+
+        if(percentage >= 90)
+        {
+            grade = "A";
+        }
+
+        else if ( percentage >= 80)
+        {
+            grade = "B";
+        }
+
+        else if ( percentage >= 70)
+        {
+            grade = "C";
+        }
+
+        else if ( percentage >= 60)
+        {
+            grade = "D";
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("More Effort needed!");
+        }
     }
 }
