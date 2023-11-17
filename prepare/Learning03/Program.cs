6081e14400358d9c8bca682419666081b028c2a5
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-17 06:07:35 +0300

    modified to accomodate the GetStringFraction and GetDecimalValue

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,24 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create instances for the three constructors(modified for step 5)
-        Fraction fraction = new Fraction();
+        // Create instances using different constructors
+        Fraction fraction1 = new Fraction();      // 1/1
+        Fraction fraction2 = new Fraction(5);     // 5/1
+        Fraction fraction3 = new Fraction(3, 4);  // 3/4
+        Fraction fraction4 = new Fraction(1, 3);  // 1/3
 
-        // Step 5
-
-        // Display intial values
-        DisplayFraction("Initial Fraction", fraction);
-
-        // Change the values using setters
-        fraction.SetTop(5);
-        fraction.SetBottom(2);
-
-        // Display the new values
-        DisplayFraction("New Values", fraction);
+        // Display representations for each fraction
+        DisplayFraction("Fraction 1", fraction1);
+        DisplayFraction("Fraction 2", fraction2);
+        DisplayFraction("Fraction 3", fraction3);
+        DisplayFraction("Fraction 4", fraction4);
     }
 
     static void DisplayFraction(string label, Fraction fraction)
     {
         Console.WriteLine($"{label}: {fraction.GetTop()}/{fraction.GetBottom()}");
+
+        // Display fraction as a string
+        Console.WriteLine($"String Representation: {fraction.GetFractionString()}");
+
+        // Display decimal value of the fraction
+        Console.WriteLine($"Decimal Value: {fraction.GetDecimalValue()}\n");
     }
 }
