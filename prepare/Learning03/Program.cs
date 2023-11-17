ed1d32c81669de093c9746d9cf2650ce1841b452
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-17 05:42:53 +0300

    changed the code to display appropriately

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,17 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create instances for the three constructors
         Fraction fraction1 = new Fraction();
-        Console.WriteLine(fraction1);
-
         Fraction fraction2 = new Fraction(6);
-        Console.WriteLine(fraction2);
-
         Fraction fraction3 = new Fraction(6, 7);
-        Console.WriteLine(fraction3);
-        
-        
-        
 
+        // Display representation for each fraction
+        DisplayFraction("Fraction 1", fraction1);
+        DisplayFraction("Fraction 1", fraction2);
+        DisplayFraction("Fraction 1", fraction3);
+    }
+
+    static void DisplayFraction(string label, Fraction fraction)
+    {
+        Console.WriteLine($"{label}: {fraction.GetTop()}/{fraction.GetBottom()}");
     }
 }
