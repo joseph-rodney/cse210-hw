db3c53eeeb4a61463366d5ddcf5913a8a11d5dfb
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-17 05:15:40 +0300

    changed order of the code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,12 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(6);
-        Fraction fraction3 = new Fraction(6, 7);
-
         Console.WriteLine(fraction1);
+
+        Fraction fraction2 = new Fraction(6);
         Console.WriteLine(fraction2);
+
+        Fraction fraction3 = new Fraction(6, 7);
         Console.WriteLine(fraction3);
+        
+        
+        
 
     }
 }
