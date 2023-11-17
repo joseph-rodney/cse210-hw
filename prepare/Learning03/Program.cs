65002da14bfa9a1c186fd58f65c2f98df4fb4b2a
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-17 05:13:35 +0300

    verify that you can create fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        Console.WriteLine(fraction1);
+        Console.WriteLine(fraction2);
+        Console.WriteLine(fraction3);
+
     }
 }
