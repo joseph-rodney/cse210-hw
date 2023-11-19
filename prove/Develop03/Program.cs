ead828349232a5a95bd6678ce1921cd2c7868cbe
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-19 07:40:47 +0300

    added code to the Program.cs file

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Craete a Scripture instance with a text and reference
+        Scripture scripture = new Scripture("In the beginning God Created Heaven and Earth.", "Genesis", 1, 1);
+
+        // Display the intital scripture text
+        Console.WriteLine("Original Scripture:");
+        Console.WriteLine(scripture.GetDisplayText());
+
+        // Hide a random word
+        Console.WriteLine("\nHiding a random word:");
+        scripture.HideRandomWords(1);
+        Console.WriteLine(scripture.GetDisplayText());
+
+        // Reveal all the words
+        Console.WriteLine("Revealing all words:");
+        scripture.RevealAllWords();
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
