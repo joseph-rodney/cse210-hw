1624d668f1a26ac517b093759f10cef9ecf66313
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-19 07:15:41 +0300

    added code to the Word.cs file

---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,28 @@
+using System;
+
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? "_____": _text;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Reveal()
+    {
+        _isHidden = false;
+    }
+}
