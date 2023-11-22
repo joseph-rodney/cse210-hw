5abe1f6f2c2765fa18d3b4ed5965ddaffbe956da
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-22 13:12:19 +0300

    added the WritingAssignment class

---
+++ prepare/Learning04/WriteAssignment.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class WriteAssignment : Assignment
+{
+    private string _title;
+
+    public WriteAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {_studentName}";
+    }
+
+}
