98a6716343222d9a62dc20b309008ae7f58da2c4
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-22 11:47:16 +0300

    completed step three of the homework

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Creating a simple assignment
+        Assignment assignment = new Assignment("Kaneene", "Inheritance");
+
+        // Calling the method to get the summary
+        string summary = assignment.GetSummary();
+
+        // Display the summary to the screen
+        Console.WriteLine(summary);
     }
 }
