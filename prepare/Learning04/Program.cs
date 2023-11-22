89e47317b3bcc600717acd6b4e261dab3ad942fa
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-22 12:41:22 +0300

    modifid the program class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,13 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Creating a simple assignment
+        // Creating a new assignment
         Assignment assignment = new Assignment("Kaneene", "Inheritance");
+        // Creating a new mathAssignment
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
 
-        // Calling the method to get the summary
+        // Testing GetSummary method
         string summary = assignment.GetSummary();
+        string summary1 = mathAssignment.GetSummary();
 
-        // Display the summary to the screen
+        // Testing GetHomeworkList method
+        string homeworklist = mathAssignment.GetHomeworkList();
+
+
+        // Display the summary and homeworklist to the screen
         Console.WriteLine(summary);
+        Console.WriteLine(summary1);
+        Console.WriteLine(homeworklist);
+
     }
 }
