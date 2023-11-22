e89b4dec82d80843a5f3cf3a0eefb31db2d796c1
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-22 13:12:39 +0300

    modified the class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -8,6 +8,8 @@ class Program
         Assignment assignment = new Assignment("Kaneene", "Inheritance");
         // Creating a new mathAssignment
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        // Creating a new writingassignment
+        WriteAssignment writeAssignment = new WriteAssignment("Mary Waters", "European History", "The Causes of World War II");
 
         // Testing GetSummary method
         string summary = assignment.GetSummary();
@@ -16,11 +18,15 @@ class Program
         // Testing GetHomeworkList method
         string homeworklist = mathAssignment.GetHomeworkList();
 
+        // Testing the WritingAssignment method
+        string writinginformation = writeAssignment.GetWritingInformation();
+
 
         // Display the summary and homeworklist to the screen
         Console.WriteLine(summary);
         Console.WriteLine(summary1);
         Console.WriteLine(homeworklist);
+        Console.WriteLine(writinginformation);
 
     }
 }
