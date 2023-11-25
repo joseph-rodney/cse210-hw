879b835499d786a59cdbd8594ab691cab5b05561
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-25 16:03:07 +0300

    display the menu

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool continueProgram = true;
+
+        while(continueProgram)
+        {
+            // Display the Menu
+            Console.WriteLine("Menu");
+            Console.WriteLine("====");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflection Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Select a choice from the menu: ");
+        }
     }
 }
