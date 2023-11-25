c9009bc533d1c7ab428d059ab9d4f99fe267ad13
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-25 13:12:33 +0300

    added logic to the base class

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,47 @@
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"Starting {_name} activity: {_description}");
+        ShowCountDown(3);
+        Console.WriteLine("{Prepare to begin...}");
+        ShowCountDown(2);
+        Console.WriteLine("Go");
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine($"Congratulations! You have completed the {_name} activity.");
+        Console.WriteLine($"Activity Duration: {_duration}");
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        for (int i = seconds; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.WriteLine($"Countdown: {i}");
+            Thread.Sleep(1000);
+        }
+    }
+}
