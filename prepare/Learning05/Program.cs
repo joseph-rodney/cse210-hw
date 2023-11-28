c8add0d4d19a6a070353704aee53d6707305ef10
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-28 13:26:11 +0300

    testing shape class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square(3);
+        square.GetArea();
+        Console.WriteLine(square);
     }
 }
