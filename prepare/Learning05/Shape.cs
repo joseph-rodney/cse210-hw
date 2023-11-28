8994a784ce63968a9ee34bf9f4fccae0894d20f4
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-28 15:16:50 +0300

    made the base class abstract

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks.Dataflow;
 
-public class Shape{
+public abstract class Shape{
     // color member variable
     protected string _color;
 
@@ -20,9 +20,18 @@ public class Shape{
         _color = color;
     }
 
-    public virtual double GetArea()
-    {
-        return 1;
-    }
+    public abstract double GetArea();
+    // Because it does not make sense to define a default body for this method in the
+    // base class, rather than make a "virtual" function here like this:
+    //
+    // public virtual double GetArea()
+    // {
+    //     return 0;
+    // }
+    //
+    // We can instead declare the function as an "abstract" function. That means
+    // that it is an empty virtual function that must be implemented or "filled in" by
+    // any class that inherits from Shape. Then, any class that has an abstract method
+    // must also be declared to be abstract.
 
 }
