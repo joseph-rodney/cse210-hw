58f3b82a1bea0881621839c29f51fd8a7049cab3
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-28 15:27:52 +0300

    removed extra methods

--- prepare/Learning05/Rectangle.cs
+++ prepare/Learning05/Rectangle.cs
@@ -3,30 +3,11 @@ public class Rectangle : Shape
     private double _length;
     private double _width;
 
-    // Get length
-    public double GetLength()
-    {
-        return _length;
-    }
-
-    //Set Length
-    public void SetLength(double length)
+    public Rectangle(string color, double length, double width): base(color)
     {
         _length = length;
-    }
-
-    //Get Width
-    public double GetWidth()
-    {
-        return _width;
-    }
-
-    //Set Width
-    public void SetWith(double width)
-    {
         _width = width;
     }
-
     //Override SetArea method
     public override double GetArea()
     {
