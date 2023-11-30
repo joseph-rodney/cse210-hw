14636825420586e7170d195b045f053cb64b918c
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-30 18:48:12 +0300

    added the logic to the base class

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,24 @@
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string shortName, string description, int points)
+    {
+        _shortName = shortName;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+
+    public abstract bool IsComplete();
+
+    public virtual string GetDetailString()
+    {
+        return $"{_shortName}: {_description} ({_points} points)";
+    }
+
+    public abstract string GetStringRepresentation();
+}
