170292b00c5de7fcc2232e3bee0768a496c4ef70
joseph-rodney <kaneenejoseph@gmail.com>
2023-11-30 19:47:28 +0300

    added the menu code

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager goalManager = new GoalManager();
+        goalManager.Start();
+
+        while(true)
+        {
+            Console.WriteLine("You have 0 points\n");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine(" 1. Create New Goal");
+            Console.WriteLine(" 2. List Goals");
+            Console.WriteLine(" 3. Save Goals");
+            Console.WriteLine(" 4. Load Goals");
+            Console.WriteLine(" 5. Record Event");
+            Console.WriteLine(" 6. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    goalManager.CreateGoal();
+                    break;
+                case "2":
+                    goalManager.ListGoalDetails();
+                    break;
+                case "3":
+                    goalManager.SaveGoals();
+                    break;
+                case "4":
+                    goalManager.LoadGoals();
+                    break;
+                case "5":
+                    goalManager.RecordEvent();
+                    break;
+                case "6":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
