9aeccd30461f8c309a253247da1087d07c3a6be3
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-08 06:16:04 +0300

    added comment class with logic

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,12 @@
+public class Comment
+{
+    //Atributes
+    public string _commenterName ;
+    public string _commentText;
+
+    public Comment(string commenterName, string commentText)
+    {
+        _commenterName = commenterName;
+        _commentText = commentText;
+    }
+}
