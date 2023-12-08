64b108732cb4bf98f703b441e3c0d45483269e13
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-08 06:17:09 +0300

    the main class displays videos and comments

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+       List<Video> videos = new List<Video>();
+            // Create videos and add comments
+            Video video1 = new Video("Avengers", "Ruso Brothers", 120);
+            video1.AddComment("Kaneene", "Black Panter was incredible!");
+            video1.AddComment("Ssemambo", "The story line was great.");
+
+            Video video2 = new Video("Black Panther", "Chadwick Boseman", 150);
+            video2.AddComment("Kyoyagala", "RIP Chadwick");
+            video2.AddComment("Nakanwagi", "Your name will live forever Chadwick Boseman");
+
+            //Add videos to the list
+            videos.Add(video1);
+            videos.Add(video2);
+
+            //Display video information
+            foreach(var video in videos)
+            {
+                Console.WriteLine($"Title: {video._title}");
+                Console.WriteLine($"Author: {video._author}");
+                Console.WriteLine($"Length: {video._length} seconds");
+                Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (var comment in video._comments)
+            {
+                Console.WriteLine($"Comment by {comment._commenterName}: {comment._commentText}");
+            }
+
+            Console.WriteLine();
+            }
     }
 }
