2c76bfc9a672b95db3cb31da11ec150920d97120
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 12:47:27 +0300

    added logic for the OutdoorGathering class

--- final/Foundation3/OutdoorGathering.cs
+++ final/Foundation3/OutdoorGathering.cs
@@ -1,4 +1,14 @@
-public class OutdoorGathering
+public class OutdoorGathering:Event
 {
-    
+    private string _weatherForecasting;
+
+    public OutdoorGathering(string weatherForecasting, string eventTitle, string description, DateTime date, TimeSpan time, Address venueAddress): base(eventTitle, description, date, time, venueAddress)
+    {
+        _weatherForecasting = weatherForecasting;
+    }
+
+    public override string FullDetails()
+    {
+        return base.FullDetails() + $"\nWeather Forecast: {_weatherForecasting}";
+    }
 }
