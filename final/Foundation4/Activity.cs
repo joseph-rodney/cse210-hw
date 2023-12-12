b5e472f9d4cb4da69e8aef07427ae0212f90b2c7
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 14:26:40 +0300

    addded logic to the base class

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,4 +1,32 @@
 public class Activity
 {
     
+    public DateTime Date;
+    public int LengthInMinutes;
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        Date = date;
+        LengthInMinutes = lengthInMinutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0.0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"Date: {Date}, Length: {LengthInMinutes} minutes";
+    }
 }
