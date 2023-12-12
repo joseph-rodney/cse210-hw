194b5be8def3ce02135269d49bcfe0b58a68bed4
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 12:46:44 +0300

    added logic for the lecture class

--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -1,4 +1,19 @@
-public class Lectures
+public class Lectures: Event
 {
-    
+
+    //Attributes
+    private string _speaker;
+    private int _capacity;
+
+    public Lectures(string speaker, int capacity, string eventTitle, string description, DateTime date, TimeSpan time, Address venueAddress): base(eventTitle, description, date, time, venueAddress)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+
+    public override string FullDetails()
+    {
+        return base.FullDetails() + $"\nSpeaker: {_speaker}\nCapacity: {_capacity} attendees.";
+    }
+
 }
