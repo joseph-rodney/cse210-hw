dbf4ba1325f6f7a1cb29e059a5a25c03f8339bd3
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 12:47:03 +0300

    added logic for the Reception class

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -1,4 +1,17 @@
-public class Reception
+public class Reception: Event
 {
-    
+    //Attributes
+    private string _rsvpEmail;
+
+    //Behavior
+    public Reception(string rsvpEmail, string eventTitle, string description, DateTime date, TimeSpan time, Address venueAddress):base(eventTitle, description, date, time, venueAddress)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+
+    public override string FullDetails()
+    {
+        return base.FullDetails() + $"\nRSVP Email: {_rsvpEmail}";
+    }
+
 }
