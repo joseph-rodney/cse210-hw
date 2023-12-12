b49dbe30dec6441fd9fcdc951fd9b9173b84c728
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 14:33:48 +0300

    running activity logic

--- final/Foundation4/RunningActivity.cs
+++ final/Foundation4/RunningActivity.cs
@@ -1,4 +1,30 @@
-public class RunningActivity
+public class RunningActivity : Activity
 {
-    
+    public double Distance { get; set; }
+
+    public RunningActivity(DateTime date, int lengthInMinutes, double distance)
+        : base(date, lengthInMinutes)
+    {
+        Distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return Distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return Distance / (LengthInMinutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return LengthInMinutes / Distance;
+    }
+
+    public override string GetSummary()
+    {
+        return base.GetSummary() + $", Distance: {Distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} minutes/mile";
+    }
 }
