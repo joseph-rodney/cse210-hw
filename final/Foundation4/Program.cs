cffb71ad34e5de776f2c20d7af0992f30142412b
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 16:46:08 +0300

    testing classes

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Create instances of activities
+        Activity runningActivity = new RunningActivity(new DateTime(2022, 11, 3), 30, 3.0);
+        Activity cyclingActivity = new CyclingActivity(new DateTime(2022, 11, 3), 30, 15.0);
+        Activity swimmingActivity = new SwimmingActivity(new DateTime(2022, 11, 3), 30, 10);
+
+        // Put activities in a list
+        List<Activity> activities = new List<Activity>
+        {
+            runningActivity,
+            cyclingActivity,
+            swimmingActivity
+        };
+
+        // Iterate through the list and display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
