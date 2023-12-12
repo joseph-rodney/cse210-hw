5a6a145ee7fb142e530e81ba1b931bacc589e411
joseph-rodney <kaneenejoseph@gmail.com>
2023-12-12 16:45:18 +0300

    added class logic

--- final/Foundation4/CyclingActivity.cs
+++ final/Foundation4/CyclingActivity.cs
@@ -1,4 +1,31 @@
-public class CyclingActivity
+public class CyclingActivity : Activity
 {
-    
+    // Attribute
+    public double _speed;
+
+    //Behavior
+    public CyclingActivity(DateTime date, int lengthInMinutes, double speed) : base(date, lengthInMinutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return _speed * (_lengthInMinutes /60.0);
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60.0 / _speed;
+    }
+
+    public override string GetSummary()
+    {
+        return base.GetSummary() + $", Speed: {_speed} mph, Distance: {GetDistance()} miles, Pace: {GetPace()} minutes/mile";
+    }
 }
